1ffd424439ffa5b59a1c00dbf884a3423da59327
Gael Pasgrimaud <gael@gawel.org>
2019-05-16 12:15:27 +0200

    ensure we have the real project name

--- gitlab/guess_test.go
+++ gitlab/guess_test.go
@@ -10,15 +10,15 @@ import (
 
 func TestGit(t *testing.T) {
 	server, project := httpUrl("https://user:pass@gitlab.com/factory/factory-cli.git")
-	assert.True(t, strings.HasSuffix(project, "factory-cli"))
+	assert.True(t, strings.HasSuffix(project, "factory/factory-cli"))
 	assert.True(t, strings.HasSuffix(server, "gitlab.com"))
 
 	server, project = gitUrl("gitlab@gitlab.com:factory/factory-cli.git")
-	assert.True(t, strings.HasSuffix(project, "factory-cli"))
+	assert.True(t, strings.HasSuffix(project, "factory/factory-cli"))
 	assert.True(t, strings.HasSuffix(server, "gitlab.com"))
 
 	_, project, err := GitRemote()
 	assert.NoError(t, err)
 	fmt.Printf("%#v", project)
-	assert.True(t, strings.HasSuffix(project, "factory-cli"))
+	assert.True(t, strings.HasSuffix(project, "factory/factory-cli"))
 }
