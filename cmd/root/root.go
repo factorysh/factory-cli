1dc7d75aa67ece1eea3e076b88b0741d248846c9
Mathieu Lecarme <mlecarme@bearstech.com>
2019-05-27 21:23:48 +0200

    ASCII art.

--- cmd/root/root.go
+++ cmd/root/root.go
@@ -35,23 +35,20 @@ var (
 	client    *http.Client
 )
 
-// RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
-	Use:   "factory-cli",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	Use:   "factory",
+	Short: "Factory command line interface",
+	Long: `
+ _
+| |             __            _
+| | /| /| /|   / _| __ _  ___| |_ ___  _ __ _   _
+| |/ |/ |/ |  | |_ / _' |/ __| __/ _ \| '__| | | |
+|          |  |  _| (_| | (__| || (_) | |  | |_| |
++----------+  |_|  \__,_|\___|\__\___/|_|   \__, |
+                                             |___/
+`,
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := RootCmd.Execute(); err != nil {
 		log.WithError(err).Error()
@@ -71,9 +68,6 @@ func init() {
 	filenameHook := filename.NewHook()
 	log.AddHook(filenameHook)
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
 	default_gitlab, default_project, _ := _gitlab.GitRemote()
 	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.factory-cli.yaml)")
 	RootCmd.PersistentFlags().StringVarP(&GitlabUrl, "gitlab", "g", default_gitlab, "Gitlab server")
