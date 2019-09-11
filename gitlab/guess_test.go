a5fe50277e506e440a5ab1453205541c2a7b2975
Mathieu Lecarme <mlecarme@bearstech.com>
2019-09-11 20:51:21 +0200

    Fix: github rulez.

--- gitlab/guess_test.go
+++ gitlab/guess_test.go
@@ -9,16 +9,16 @@ import (
 )
 
 func TestGit(t *testing.T) {
-	server, project := httpUrl("https://user:pass@gitlab.com/factory/factory-cli.git")
-	assert.True(t, strings.HasSuffix(project, "factory/factory-cli"))
-	assert.True(t, strings.HasSuffix(server, "gitlab.com"))
+	server, project := httpUrl("https://user:pass@github.com/factorysh/factory-cli.git")
+	assert.True(t, strings.HasSuffix(project, "factorysh/factory-cli"))
+	assert.True(t, strings.HasSuffix(server, "github.com"))
 
-	server, project = gitUrl("gitlab@gitlab.com:factory/factory-cli.git")
-	assert.True(t, strings.HasSuffix(project, "factory/factory-cli"))
-	assert.True(t, strings.HasSuffix(server, "gitlab.com"))
+	server, project = gitUrl("git@github.com:factorysh/factory-cli.git")
+	assert.True(t, strings.HasSuffix(project, "factorysh/factory-cli"))
+	assert.True(t, strings.HasSuffix(server, "github.com"))
 
 	_, project, err := GitRemote()
 	assert.NoError(t, err)
 	fmt.Printf("%#v", project)
-	assert.True(t, strings.HasSuffix(project, "factory/factory-cli"))
+	assert.True(t, strings.HasSuffix(project, "factorysh/factory-cli"))
 }
