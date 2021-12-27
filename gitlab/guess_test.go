6adefa2eea7ac406b3a5643246df16c16e49f226
Gael Pasgrimaud <gael@gawel.org>
2021-12-27 16:29:02 +0100

    cant be splitted...?

--- gitlab/guess_test.go
+++ gitlab/guess_test.go
@@ -12,15 +12,11 @@ func TestGitHttp(t *testing.T) {
 	server, project := httpUrl("https://user:pass@github.com/factorysh/factory-cli.git")
 	assert.True(t, strings.HasSuffix(project, "factorysh/factory-cli"))
 	assert.True(t, strings.HasSuffix(server, "github.com"))
-}
 
-func TestGitSsh(t *testing.T) {
 	server, project = gitUrl("git@github.com:factorysh/factory-cli.git")
 	assert.True(t, strings.HasSuffix(project, "factorysh/factory-cli"))
 	assert.True(t, strings.HasSuffix(server, "github.com"))
-}
 
-func TestGitRemote(t *testing.T) {
 	_, project, err := GitRemote()
 	assert.NoError(t, err)
 	fmt.Printf("%#v", project)
