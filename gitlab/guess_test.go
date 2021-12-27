07b510886315bbba89194b952cb20bf5c1134586
Gael Pasgrimaud <gael@gawel.org>
2021-12-27 16:26:13 +0100

    split tests

--- gitlab/guess_test.go
+++ gitlab/guess_test.go
@@ -8,15 +8,19 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGit(t *testing.T) {
+func TestGitHttp(t *testing.T) {
 	server, project := httpUrl("https://user:pass@github.com/factorysh/factory-cli.git")
 	assert.True(t, strings.HasSuffix(project, "factorysh/factory-cli"))
 	assert.True(t, strings.HasSuffix(server, "github.com"))
+}
 
+func TestGitSsh(t *testing.T) {
 	server, project = gitUrl("git@github.com:factorysh/factory-cli.git")
 	assert.True(t, strings.HasSuffix(project, "factorysh/factory-cli"))
 	assert.True(t, strings.HasSuffix(server, "github.com"))
+}
 
+func TestGitRemote(t *testing.T) {
 	_, project, err := GitRemote()
 	assert.NoError(t, err)
 	fmt.Printf("%#v", project)
